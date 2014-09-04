7291d4bc903e596bd65dc830d9fa9b530008c2bf
Tony Plaza <tplaza3@gmail.com>
2014-09-04 09:46:12 -0500

    added click conditional and cats game!

--- tic-tac-toe/ttt.js
+++ tic-tac-toe/ttt.js
@@ -2,42 +2,39 @@ window.onload = function(){
   
   // Collection of all squares on the board.
   var squares = document.getElementsByTagName('td');
-  
+  // Counts the moves
   var counter = 0
-	
- 
   
   function add_mark(){
-		if(counter%2 == 0){
+		if(counter%2 == 0 && this.innerHTML == "-"){
 		 var turn = "X";
 		 this.innerHTML = turn;
 		 counter++;
-   
 		 winning_move();
 		 
-		} else {
+		} else if(this.innerHTML == "-"){
 			var turn = "O";
 			this.innerHTML = turn;
 			counter++;
 			winning_move();
+		} else if(counter == 8){
+			alert("Cat's Game!!!!")
 		}
 		
   }
 	
 	function winning_move() {
-    
-  	var tl = document.getElementById("TL").textContent;
-  	var tm = document.getElementById("TM").textContent;
-  	var tr = document.getElementById("TR").textContent;
-  	var ml = document.getElementById("ML").textContent;
-  	var mm = document.getElementById("MM").textContent;
-  	var mr = document.getElementById("MR").textContent;
-  	var bl = document.getElementById("BL").textContent;
-  	var bm = document.getElementById("BM").textContent;
-  	var br = document.getElementById("BR").textContent;
-	
-    
-    
+		
+		var tl = document.getElementById("TL").textContent;
+		var tm = document.getElementById("TM").textContent;
+		var tr = document.getElementById("TR").textContent;
+		var ml = document.getElementById("ML").textContent;
+		var mm = document.getElementById("MM").textContent;
+		var mr = document.getElementById("MR").textContent;
+		var bl = document.getElementById("BL").textContent;
+		var bm = document.getElementById("BM").textContent;
+		var br = document.getElementById("BR").textContent;
+	  
 		if((tl === tm && tm === tr && tr !== "-") || (ml === mm && mm  === mr && mr !== "-") || (bl === bm && bm ===br && br !== "-") || (tl === ml && ml === bl&& bl !== '-') || 
 			 (tm === mm && mm === bm && bm !== '-') || (tr === mr && mr === br && br !== '-') || (tl === mm && mm === br && br !== '-') || (tr === mm && mm === bl && bl !== '-')){
 				 alert("WINNNNNNNNSSSSSSSSSSSSSSSSS!!!!!!!!!!");
